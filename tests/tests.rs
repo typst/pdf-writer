849f0ccad5a61345c374e7e6df163f5a397993be
Laurenz <laurmaedje@gmail.com>
2021-05-22 18:30:47 +0200

    Array and dictionary tests

--- tests/tests.rs
+++ tests/tests.rs
@@ -1,4 +1,4 @@
-use pdf_writer::{Name, Null, PdfWriter, Ref, Str};
+use pdf_writer::{IndirectGuard, Name, Null, Obj, PdfWriter, Ref, Str};
 
 /// Test that `buf` is the same as the result of concatenating the strings.
 macro_rules! test {
@@ -9,16 +9,22 @@ macro_rules! test {
     }}
 }
 
-/// Test how a basic object is written.
-macro_rules! test_primitive {
-    ($basic:expr, $($tts:tt)*) => {{
-        let buf = with(|w| w.indirect(Ref::new(1)).primitive($basic));
-        test!(&buf[8 .. buf.len() - 9], $($tts)*)
+/// Test how an object is written.
+macro_rules! test_obj {
+    (|$obj:ident| $write:expr, $($tts:tt)*) => {{
+        test!(slice_obj(|$obj| { $write; }), $($tts)*)
     }}
 }
 
+/// Test how a primitive object is written.
+macro_rules! test_primitive {
+    ($value:expr, $($tts:tt)*) => {
+        test_obj!(|obj| obj.primitive($value), $($tts)*);
+    }
+}
+
 /// Return the slice of bytes written during the execution of `f`.
-fn with<F>(f: F) -> Vec<u8>
+fn slice<F>(f: F) -> Vec<u8>
 where
     F: FnOnce(&mut PdfWriter),
 {
@@ -28,6 +34,15 @@ where
     let end = w.len();
     let buf = w.finish(Ref::new(1));
     buf[start .. end].to_vec()
+}
+
+/// Return the slice of bytes written for an object.
+fn slice_obj<F>(f: F) -> Vec<u8>
+where
+    F: FnOnce(Obj<IndirectGuard>),
+{
+    let buf = slice(|w| f(w.indirect(Ref::new(1))));
+    buf[8 .. buf.len() - 9].to_vec()
 }
 
 #[test]
@@ -44,6 +59,15 @@ fn test_minimal() {
 }
 
 #[test]
+#[should_panic(expected = "unfinished object")]
+#[allow(unused_must_use)]
+fn test_object_unused() {
+    let mut w = PdfWriter::new(1, 7);
+    w.indirect(Ref::new(1));
+    w.finish(Ref::new(1));
+}
+
+#[test]
 fn test_primitive_objects() {
     test_primitive!(true, "true");
     test_primitive!(false, "false");
@@ -53,4 +77,28 @@ fn test_primitive_objects() {
     test_primitive!(Name(b"Filter"), "/Filter");
     test_primitive!(Ref::new(7), "7 0 R");
     test_primitive!(Null, "null");
+}
+
+#[test]
+fn test_arrays() {
+    test_obj!(|obj| obj.array(), "[]");
+    test_obj!(|obj| obj.array().item(12).item(Null), "[12 null]");
+    test_obj!(|obj| obj.array().typed().items(vec![1, 2, 3]), "[1 2 3]");
+    test_obj!(
+        |obj| {
+            let mut array = obj.array();
+            array.obj().array().typed().items(vec![1, 2]);
+            array.item(3);
+        },
+        "[[1 2] 3]",
+    );
+}
+
+#[test]
+fn test_dicts() {
+    test_obj!(|obj| obj.dict(), "<<\n>>");
+    test_obj!(
+        |obj| obj.dict().pair(Name(b"Quality"), Name(b"Good")),
+        "<<\n/Quality /Good\n>>",
+    );
 }
