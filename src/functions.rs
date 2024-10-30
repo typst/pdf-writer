2c80827535d6422b69dd0f4efb337f72f71f3601
Laurenz Stampfl <laurenz.stampfl+github@gmail.com>
2024-10-30 22:18:50 +0100

    Fix test

--- src/functions.rs
+++ src/functions.rs
@@ -446,7 +446,7 @@ mod tests {
         ];
 
         assert_eq!(
-            PostScriptOp::encode(&ops),
+            &PostScriptOp::encode(&ops).to_bytes(),
             b"{ 3.0 2.0 mul exch dup 0.0 ge { 1.0 add } {neg} ifelse add }"
         );
     }
